bcaac94237b0f70233ffe1d694391ed9b9dc2ef6
someCat <zhangtao@touchdata.io>
2020-03-31 11:19:46 +0800

    项目初始化

---
+++ src/main/index.js
@@ -0,0 +1,81 @@
+'use strict'
+
+import { app, BrowserWindow } from 'electron'
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080`
+  : `file://${__dirname}/index.html`
+
+function createWindow () {
+  /**
+   * Initial window options
+   */
+
+  mainWindow = new BrowserWindow({
+    // height: 563,
+    // useContentSize: true,
+    // width: 1000,
+    // transparent: true // 设置透明
+    height: 580,
+    width: 300,
+    useContentSize: true,
+    frame: false, // 要创建无边框窗口
+    resizable: false, // 禁止窗口大小缩放
+    webPreferences: {
+      devTools: false // 关闭调试工具
+    },
+    transparent: true, // 设置透明
+    alwaysOnTop: true // 窗口是否总是显示在其他窗口之前
+  })
+
+  mainWindow.loadURL(winURL)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    setTimeout(function () {
+      createWindow()
+    }, 10)
+  }
+})
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
---
+++ src/renderer/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'zhongxiong'
+  }
+</script>
+
+<style>
+  /* CSS */
+  canvas{
+    border: none !important
+  }
+</style>
